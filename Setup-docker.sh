3c8e8688152b44fec226c54bd30ef8dea0100265
Priscillia ARRACHARD <parracha@made-f0Br4s9.clusters.42paris.fr>
2024-01-29 18:45:40 +0100

    done

--- Setup-docker.sh
+++ Setup-docker.sh
@@ -17,6 +17,8 @@ sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+sleep 15
+
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
