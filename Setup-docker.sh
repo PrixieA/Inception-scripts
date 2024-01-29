2d2ba2046b9cc2fa53d19fba8ca49317c4e81298
Priscillia ARRACHARD <parracha@made-f0Br4s9.clusters.42paris.fr>
2024-01-29 18:56:27 +0100

    done

--- Setup-docker.sh
+++ Setup-docker.sh
@@ -11,8 +11,8 @@ clear
 # Installation
 
 # Add Docker's official GPG key:
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg -y
+sudo apt-get update 
+sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -24,7 +24,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
+sudo apt-get update
 
 sleep 15
 
